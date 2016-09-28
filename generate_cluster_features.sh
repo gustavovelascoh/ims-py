0c6e63cf0dd1963f4dcca9867e58008a1ad961de
Gustavo Velasco-Hernández <gustavo.velascoh@gmail.com>
2016-09-27 21:02:21 -0500

    Added samplig count in generate_features script

--- generate_cluster_features.sh
+++ generate_cluster_features.sh
@@ -2,12 +2,21 @@
 
 files=$(ls *.clus)
 #echo $files
-
+init_samp=0
 > cluster_features
 
 for file in $files
 do
-	cmd="python3 cluster_analysis.py $file --features --noplot >> cluster_features"
+	cmd="python3 cluster_analysis.py $file --features --noplot > feat_tmp"
+	copy="cat feat_tmp >> cluster_features"
 	echo "Executing $cmd"
 	eval $cmd
+	eval $copy
+	n_samp=$(cat feat_tmp | wc -l)
+	echo "Total samples: $n_samp"
+	end_samp=$((init_samp+n_samp-1))
+	echo "features $init_samp to $end_samp"
+	init_samp=$((end_samp+1))
 done
+
+rm feat_tmp
