74ed10df6b987e4890e7b2bac51beb19610fedce
Gustavo Velasco-Hernández <gustavo.velascoh@gmail.com>
2017-01-18 21:58:58 -0500

    Removed commented code

--- gui/viewer.py
+++ gui/viewer.py
@@ -55,54 +55,6 @@ class Viewer(tk.Frame):
         self.canvas.show()
         self.background = self.fig.canvas.copy_from_bbox(self.ax.bbox)
                 
-        # tk.Frame.__init__(self, master)
-        
-#         button_frame = tk.Frame(self)
-#         nbutton = tk.Button(button_frame, text="Next",
-#                            command=self._next_frame)
-#         nbutton.pack(side="left")
-#         lbutton = tk.Button(button_frame, text="Loop",
-#                            command=self._loop)
-#         lbutton.pack(side="left")
-#         sbutton = tk.Button(button_frame, text="Stop",
-#                            command=self._stop)
-#         sbutton.pack(side="left")
-#         button_frame.pack(side="top")
-#         
-#         self.new_data_cb = None
-#         self.ts = 0
-#         
-#         
-#         
-#     def _loop(self):
-#         self.loop = True
-#         t1 = threading.Thread(target=self._loop_thread)
-#         t1.start()
-#         
-#         
-#             
-#     def _loop_thread(self):
-#         while self.loop:
-#             last_ts = self.ts
-#             print("clear")
-#             data, last, self.ts = self.new_data_cb()
-#             diff = self.ts - last_ts
-#             time.sleep(diff/1000.0)
-#             self.plot_data.set_data(data[:,0], data[:,1])
-#             self.ax.draw_artist(self.plot_data)
-#             self.canvas.show()     
-#     
-#     def _stop(self):
-#         self.loop = False
-#         
-#     def _next_frame(self):
-#         #self.ax.clear()
-#         #self.canvas.show()
-#         print("clear")
-#         data, last, self.ts = self.new_data_cb()
-#         self.plot_data.set_data(data[:,0], data[:,1])
-#         self.ax.draw_artist(self.plot_data)
-#         self.canvas.show()
 
 if __name__ == "__main__":
     main = tk.Tk()
