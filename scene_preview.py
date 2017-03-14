313841f4e694806f9e0bed1cdeb6fe6751e9149c
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2017-03-13 23:17:57 -0500

    Added legs drawing on scene preview

--- scene_preview.py
+++ scene_preview.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 from time import sleep
+import matplotlib.patches as patches
 
 
 img=mpimg.imread('possi/image.bmp')
@@ -34,6 +35,32 @@ angs = [0.150098, -3.344051, -4.174827, -0.059341, 3.679154, -1.933825]
 ax.plot(xos,yos, '.', markerfacecolor='b', markeredgecolor='k', markersize=10)
 
 
-plt.show()
+#plt.show()
 
-sleep(15)
+import json
+
+json_data=open("possi.legs").read()
+
+data = json.loads(json_data)
+
+for leg in data:
+    
+    minx = leg['bbox'][0]
+    miny = leg['bbox'][1]
+    dx = leg['bbox'][2] - minx
+    dy = leg['bbox'][3] - miny
+    
+    color = "purple" if leg["type"] == "departure" else "cyan"
+    
+    ret = ax.add_patch(
+         patches.Rectangle(
+            (minx, miny),
+            dx,
+            dy,
+            fill=False,      # remove background
+            edgecolor=color
+         ) )
+    
+    print (ret)
+plt.show()
+#sleep(15)
