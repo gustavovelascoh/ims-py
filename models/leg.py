27de53841c1af11d719a2e73c7bf7c13fdf16be7
gustavo.velasco <gustavo.velasco@colbits.com.co>
2017-05-25 10:24:56 -0500

    Added methods for leg handling

--- models/leg.py
+++ models/leg.py
@@ -20,3 +20,17 @@ class Leg(object):
         self.heading = heading
         self.bbox = BoundingBox((bbox[0],bbox[1]),(bbox[2],bbox[3]))
         self.lanes = lanes
+        self.state = None
+        self.occupied_area = 0
+        
+    def add_item(self, item):
+        self.occupied_area += item.bbox.area
+    
+    def is_in(self, item):
+        
+        return self.bbox.is_in(item.bbox.center)
+    
+    def check_item(self, item):
+        
+        if self.is_in(item):
+            self.add_item(item)
