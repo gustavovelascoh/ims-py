0483e2cf6da0496556a131f09dee6437749bb548
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2017-12-12 22:36:22 -0500

    Plotting of data files added

--- utils/legs_state_plot.py
+++ utils/legs_state_plot.py
@@ -6,9 +6,33 @@ Created on Nov 23, 2017
 import pickle
 import numpy as np
 
-filename = "../possi_123578.imscfg.data"
+from matplotlib import pyplot as plt
 
+
+filename = "../possi_123578.cs03th06a.data"
+data = None
 with open(filename,"rb") as f:
     data = pickle.load(f)
     
 print(np.shape(data))
+print(data[0])
+
+ts = np.array([])
+legs_state = np.array([])
+
+for f in data:
+    ts = np.append(ts, f["ts"])
+    legs_array = np.array([f["legs_state"]])
+    if len(legs_state) == 0:
+        legs_state = legs_array.T
+    else:
+        legs_state = np.concatenate((legs_state,legs_array.T), axis=1)
+print(np.shape(ts))
+print(np.shape(legs_state))
+
+# plt.subplot(2,2,4)
+plt.plot(legs_state.T)
+
+plt.show()
+    
+    
