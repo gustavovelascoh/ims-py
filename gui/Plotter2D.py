3bd46d29db79774381ff7c043e7cb338037fbe99
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2018-01-18 00:36:15 -0500

    Node for 2d plotting tested

--- gui/Plotter2D.py
+++ gui/Plotter2D.py
@@ -24,6 +24,9 @@ class Plotter2D(tk.Frame):
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
         self.canvas.show()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.total_lines = 0
+        self.fixed_lines = 0
+        self.used_lines = 0
         
     def plot(self, *args, **kwargs):
         self.available_lines = self.total_lines - self.fixed_lines - self.used_lines
@@ -32,12 +35,51 @@ class Plotter2D(tk.Frame):
 #             print("Updating line ")
             line_idx = (self.fixed_lines + self.used_lines)            
             self.__update_plot_data( *args, line_idx=line_idx)
-            self.update_style(line_idx, **kwargs)
+            #self.update_style(line_idx, **kwargs)
             
         else:
             self.ax.plot(*args, **kwargs)            
-            self.update_style(line_idx="last", **kwargs)
-            self.total_lines = len(self.ax.get_lines())     
+            #self.update_style(line_idx="last", **kwargs)
+            self.total_lines = len(self.ax.get_lines())
         
+        self.used_lines += 1
+    
+    def __update_plot_data(self, *args, line_idx=0):
+        
+        #print(self.plot_data)
+        list_of_lines = self.ax.get_lines()
+        #print("Length lol %d" % len(list_of_lines))
+        curr_line = list_of_lines[line_idx]
+        if len(args) == 2:
+            curr_line.set_data(*args)
+        else:
+            curr_line.set_data(*args[0:2])
+            curr_line.set_linestyle(args[2])
+            
+        
+        self.ax.draw_artist(curr_line)  
+    
+    def update(self, xmin=None, xmax=None):
+        
+        
+        lines_to_discard = self.total_lines - self.fixed_lines - self.used_lines
+        #print("l2d: %d", lines_to_discard)
+        
+        lines = self.ax.get_lines()
+        #print("Length lol %d" % len(lines))
+        for i in range(0,lines_to_discard):
+            lines = self.ax.get_lines()
+            #print("removing...")
+            lines[-1].remove()
+        
+        if xmin:
+            self.ax.set_xlim(xmin=xmin,xmax=xmax)
+        
+        self.used_lines = 0
+        lines = self.ax.get_lines()
+        self.total_lines = len(lines)
+        
+        #print("Length lol %d" % len(lines))
+        self.canvas.draw()
         
         
--- utils/legs_state_plotter.py
+++ utils/legs_state_plotter.py
@@ -3,17 +3,56 @@ Created on Dec 25, 2017
 
 @author: gustavo
 '''
+import numpy as np
 from gui.Plotter2D import Plotter2D
+from models.subscriber import Subscriber
 import tkinter as tk
+import json
+
+BUFF_SIZE = 100
+
+time_array = np.array([])
+data_array = np.array([])
+
 
 main = tk.Tk()
 main.wm_title("Legs State Plotter")
 # Code to add widgets will go here...
-p = Plotter2D(main).pack()
+p = Plotter2D(main)
+p.pack()
 
-def subscriber_cb():
+def subscriber_cb(msg):
     
+    global time_array
+    global data_array
+    
+    print(msg)
+    
+    payload = json.loads(msg["data"].decode("utf-8"))
+    print(payload)
+   
+    time_array = np.append(time_array, payload["ts"])
+    print(time_array)
+    
+    curr_data_array = np.array([payload["data"]])
+    
+    print(np.shape(curr_data_array))
+    print(curr_data_array)
+    print(np.transpose(curr_data_array))
+    print(np.shape(data_array))
+        
+    if np.shape(data_array)[0] == 0:
+        data_array = np.transpose(curr_data_array)
+    else:
+        data_array = np.concatenate((data_array, np.transpose(curr_data_array)), axis=1)
+    
+    print(data_array)
+    
+    for dat in data_array:
+        p.plot(time_array, dat)
+    
+    p.update(xmin=time_array[0], xmax=time_array[-1])
 
-
-
+s = Subscriber({"test_topic": subscriber_cb})
+s.run()
 tk.mainloop()
