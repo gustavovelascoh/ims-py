922441f48d6252a726f741581e0fe2d2154014b4
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2018-05-29 07:07:54 -0500

    now loads laser config

--- gui/scene_config_viewer.py
+++ gui/scene_config_viewer.py
@@ -9,6 +9,7 @@ import json
 from gui.viewer import Viewer
 import tkinter as tk
 import numpy as np
+import pickle
 
 class SceneConfigViewer(tk.Frame):
     def __init__(self, master):
@@ -28,30 +29,40 @@ class SceneConfigViewer(tk.Frame):
         
         #print(self.config)
         
-        v = Viewer(self)
-        v.pack()
+        self.v = Viewer(self)
+        self.v.pack()
         
         self.__generate_limits()
         
-        v.draw_img(self.ims_path + "/" +self.config["map"]["image_path"], limits=self.limits)
+        self.v.draw_img(self.ims_path + "/" +self.config["map"]["image_path"], limits=self.limits)
         
         for int_bb in self.config["intersection"]:
             pts = int_bb["bbox"]
-            v.plot_box(pts[0], pts[1], pts[2]-pts[0], pts[3]-pts[1],edgecolor="yellow")
+            self.v.plot_box(pts[0], pts[1], pts[2]-pts[0], pts[3]-pts[1],edgecolor="yellow")
         
         for leg_bb in self.config["legs"]:
             pts = leg_bb["bbox"]
-            v.plot_box(pts[0], pts[1], pts[2]-pts[0], pts[3]-pts[1],edgecolor="blue")
+            self.v.plot_box(pts[0], pts[1], pts[2]-pts[0], pts[3]-pts[1],edgecolor="blue")
     
         rs_src_list_str = ""
         rs_name_list_str = ""
+        self.rs_config = []
         
         for rs in self.config["range_sensors"]:
             rs_src_list_str += rs["src_path"] + " "
             rs_name_list_str += rs["name"] + " "
             r.set("ims.rs."+rs["name"]+".src",rs["src_path"])
+            
+            f = open(rs["cfg_path"], "rb")
+            self.rs_config.append(pickle.load(f))
         
         r.set("ims.rs_name_list", rs_name_list_str)
+    
+    def __plot_rs(self):
+        
+        for rs in self.rs_config:
+            print("rs")
+            pass    
             
     def __generate_limits(self):
         map_scale = self.config["map"]["scale"]
